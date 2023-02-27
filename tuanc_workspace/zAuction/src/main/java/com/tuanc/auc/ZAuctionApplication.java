448adfca73030d1d5e857294b90e1d9589317910
tuancGeo <126409610+tuancGeo@users.noreply.github.com>
2023-02-28 01:50:27 +0700

    Create a Spring project
    Add a HomeCtrl
    Add a MySQL connector with database Zauction
    Add a basic Home Page Only jsp, html

---
+++ tuanc_workspace/zAuction/src/main/java/com/tuanc/auc/ZAuctionApplication.java
@@ -0,0 +1,13 @@
+package com.tuanc.auc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZAuctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZAuctionApplication.class, args);
+	}
+
+}
