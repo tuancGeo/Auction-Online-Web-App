bcd4b66e49a9b6b9746e915c1fa5231c55536385
tuancGeo <126409610+tuancGeo@users.noreply.github.com>
2023-02-28 01:57:27 +0700

    Create a Spring project
    Add a configuration for
    - jsp web : prefix, suffix, static path, port
    - mySQL and lazy load for jpa
    Create a MySQL connector and create a hibernate configuration
    Add  entities folder with class was generated by hibernate
    Simple Home page for test

---
+++ zAuction/src/test/java/com/tuanc/auc/ZAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuanc.auc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZAuctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
